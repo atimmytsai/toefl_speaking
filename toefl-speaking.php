72d6eb33969c419ea271fc39d996ff989381d699
atimmytsai <123465995+atimmytsai@users.noreply.github.com>
2023-04-30 17:22:55 +0800

    Update toefl-speaking.php

--- toefl-speaking.php
+++ toefl-speaking.php
@@ -1,124 +1,106 @@
 <?php
 /**
- * Plugin Name: TOEFL Speaking
- * Description: Creates a new CPT for TOEFL Speaking with custom taxonomies and a front-end template.
+ * Plugin Name: TOEFL Speaking CPT
+ * Description: A plugin to create a custom post type and taxonomies for TOEFL Speaking.
  * Version: 1.0
- * Author: Your Name
- * Author URI: yourwebsite.com
+ * Author: Atimmy Tsai
  */
 
-//Register the custom post type:.
-function toefl_speaking_cpt() {
+// Register Custom Post Type
+function toef_speaking_register_cpt() {
     $labels = array(
-        'name'               => __( 'TOEFL Speakings', 'toefl-speaking' ),
-        'singular_name'      => __( 'TOEFL Speaking', 'toefl-speaking' ),
-        'menu_name'          => __( 'TOEFL Speakings', 'toefl-speaking' ),
-        'name_admin_bar'     => __( 'TOEFL Speaking', 'toefl-speaking' ),
-        'add_new'            => __( 'Add New', 'toefl-speaking' ),
-        'add_new_item'       => __( 'Add New TOEFL Speaking', 'toefl-speaking' ),
-        'new_item'           => __( 'New TOEFL Speaking', 'toefl-speaking' ),
-        'edit_item'          => __( 'Edit TOEFL Speaking', 'toefl-speaking' ),
-        'view_item'          => __( 'View TOEFL Speaking', 'toefl-speaking' ),
-        'all_items'          => __( 'All TOEFL Speakings', 'toefl-speaking' ),
-        'search_items'       => __( 'Search TOEFL Speakings', 'toefl-speaking' ),
-        'parent_item_colon'  => __( 'Parent TOEFL Speakings:', 'toefl-speaking' ),
-        'not_found'          => __( 'No TOEFL Speakings found.', 'toefl-speaking' ),
-        'not_found_in_trash' => __( 'No TOEFL Speakings found in Trash.', 'toefl-speaking' )
+        'name' => __('TOEFL Speaking', 'toef-spk'),
+        'singular_name' => __('TOEFL Speaking', 'toef-spk'),
+        // ... (other labels)
     );
-
     $args = array(
-        'labels'             => $labels,
-        'public'             => true,
-        'publicly_queryable' => true,
-        'show_ui'            => true,
-        'show_in_menu'       => true,
-        'query_var'          => true,
-        'rewrite'            => array( 'slug' => 'toefl_speakings' ),
-        'capability_type'    => 'post',
-        'has_archive'        => true,
-        'hierarchical'       => false,
-        'menu_position'      => null,
-        'supports'           => array( 'title', 'editor', 'author', 'thumbnail', 'excerpt', 'comments' )
+        'label' => __('TOEFL Speaking', 'toef-spk'),
+        'labels' => $labels,
+        'supports' => array('title', 'editor', 'thumbnail'),
+        'public' => true,
+        'has_archive' => true,
     );
-
-    register_post_type( 'toefl_speakings', $args );
+    register_post_type('toef_speaking', $args);
 }
-add_action( 'init', 'toefl_speaking_cpt' );
+add_action('init', 'toef_speaking_register_cpt');
 
-// Include Metaboxes
-require_once dirname( __FILE__ ) . '/inc/toefl_speaking_metaboxes.php';
-
-// Enqueue styles and scripts
-function toefl_speaking_cpt_scripts() {
-    wp_enqueue_style( 'toefl-speaking-cpt-style', plugins_url( 'assets/css/toefl_speaking.css', __FILE__ ), array(), '1.0.0' );
-    wp_enqueue_script( 'toefl-speaking-cpt-script', plugins_url( 'assets/js/toefl_speaking.js', __FILE__ ), array( 'jquery' ), '1.0.0', true );
-}
-add_action( 'wp_enqueue_scripts', 'toefl_speaking_cpt_scripts' );
-
-
-
-// Register the custom taxonomies:
-
-function toefl_speaking_taxonomies() {
-    // Register "Task" taxonomy
+// Register Custom Taxonomies
+function toef_speaking_register_taxonomies() {
+    // Register Task Taxonomy
     $task_labels = array(
-        'name' => _x('Tasks', 'taxonomy general name'),
-        // Add more labels as needed
+        'name' => __('Tasks', 'toef-spk'),
+        'singular_name' => __('Task', 'toef-spk'),
+        // ... (other labels)
     );
-
     $task_args = array(
         'labels' => $task_labels,
         'public' => true,
         'hierarchical' => true,
-        'rewrite' => array('slug' => 'task'),
+        'rewrite' => array('slug' => 'toef_speaking_task'),
     );
+    register_taxonomy('toef_speaking_task', array('toef_speaking'), $task_args);
 
-    register_taxonomy('task', 'toefl_speakings', $task_args);
-
-
-    // Topic taxonomy
-    register_taxonomy('topic', 'toefl_speakings', array(
+    // Register Topic Taxonomy
+    $topic_labels = array(
+        'name' => __('Topics', 'toef-spk'),
+        'singular_name' => __('Topic', 'toef-spk'),
+        // ... (other labels)
+    );
+    $topic_args = array(
+        'labels' => $topic_labels,
+        'public' => true,
         'hierarchical' => true,
-        'rewrite' => array('slug' => 'topic'),
-        'labels' => array(
-        'name' => __('Topics'),
-        'singular_name' => __('Topic'),
-        'menu_name' => __('Topics'),
-        ),
-    ));
+        'rewrite' => array('slug' => 'toef_speaking_topic'),
+    );
+    register_taxonomy('toef_speaking_topic', array('toef_speaking'), $topic_args);
 
-    // Difficulty taxonomy
-    register_taxonomy('difficulty', 'toefl_speakings', array(
+    // Register Difficulty Taxonomy
+    $difficulty_labels = array(
+        'name' => __('Difficulties', 'toef-spk'),
+        'singular_name' => __('Difficulty', 'toef-spk'),
+        // ... (other labels)
+    );
+    $difficulty_args = array(
+        'labels' => $difficulty_labels,
+        'public' => true,
         'hierarchical' => true,
-        'rewrite' => array('slug' => 'difficulty'),
-        'labels' => array(
-        'name' => __('Difficulties'),
-        'singular_name' => __('Difficulty'),
-        'menu_name' => __('Difficulties'),
-        ),
-    ));
-    }
-add_action('init', 'toefl_speaking_taxonomies');
-
-
-//Add pre-built terms to "Task" taxonomy:
-function toefl_speaking_default_terms() {
-    $default_tasks = array('Task 1', 'Task 2', 'Task 3', 'Task 4');
-    foreach ($default_tasks as $task) {
-        if (!term_exists($task, 'task')) {
-            wp_insert_term($task, 'task');
-        }
-    }
+        'rewrite' => array('slug' => 'toef_speaking_difficulty'),
+    );
+    register_taxonomy('toef_speaking_difficulty', array('toef_speaking'), $difficulty_args);
 }
-add_action('init', 'toefl_speaking_default_terms');
+add_action('init', 'toef_speaking_register_taxonomies');
 
+// Enqueue Custom CSS and JS
+function toef_speaking_enqueue_scripts() {
+    wp_enqueue_style('toef-spk-style', plugin_dir_url(__FILE__) . 'css/toef-spk.css');
+    wp_enqueue_script('toef-spk-script', plugin_dir_url(__FILE__) . 'js/toef-spk.js', array('jquery'), '1.0', true);
+}
+add_action('wp_enqueue_scripts', 'toef_speaking_enqueue_scripts');
 
-// Include theme functions file
-require_once plugin_dir_path( __FILE__ ) . 'theme-functions.php';
-
-register_activation_hook( __FILE__, 'reset_permalinks' );
-function reset_permalinks() {
+// Flush rewrite rules upon plugin activation
+function toef_speaking_activate() {
+    toef_speaking_register_cpt();
+    toef_speaking_register_taxonomies();
     flush_rewrite_rules();
 }
+register_activation_hook(__FILE__, 'toef_speaking_activate');
 
-?>
+// Add archive template for TOEFL Speaking CPT
+function toef_speaking_archive_template($archive_template) {
+    global $post;
+    if (is_post_type_archive('toef_speaking')) {
+        $archive_template = plugin_dir_path(__FILE__) . 'templates/archive-toef_speaking.php';
+    }
+    return $archive_template;
+}
+add_filter('archive_template', 'toef_speaking_archive_template');
+
+// Add single template for TOEFL Speaking CPT
+function toef_speaking_single_template($single_template) {
+    global $post;
+    if ($post->post_type == 'toef_speaking') {
+        $single_template = plugin_dir_path(__FILE__) . 'templates/single-toef_speaking.php';
+    }
+    return $single_template;
+}
+add_filter('single_template', 'toef_speaking_single_template');
